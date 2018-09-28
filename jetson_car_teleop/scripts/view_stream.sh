5e146e22ff3a35be29eccfc9a41a5193748b16e0
Pavel Tarasov <tarasradio@mail.ru>
2018-09-28 19:40:24 +0300

    Create jetson_car_teleop ROS package     - launch script to get joystick commands and get       video stream from camera

---
+++ jetson_car_teleop/scripts/view_stream.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+PORT=5000
+
+while getopts ":hp:" opt
+do
+case $opt in
+h)
+	echo "./view_stream.sh [-p <port>]"
+	echo "View stream from NVIdia Jetson CSI-camera"
+	echo "    -port Streaming port, default $PORT"
+	exit 0
+	;;
+p)	POPT=$OPTARG ;;
+*)  echo "Invalid argument" ;;
+esac
+done
+
+gst-launch-1.0 udpsrc port=$PORT ! application/x-rtp,encoding-name=H264,payload=96 ! rtph264depay ! h264parse ! queue ! avdec_h264 ! xvimagesink sync=false async=false -e
