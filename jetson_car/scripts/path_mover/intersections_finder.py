Experiments. Fix bearing calculation error Now car can move around the arc

jetson_car/scripts/path_mover/intersections_finder.py
@@ -1,5 +1,5 @@
 #coding=utf-8
-
+import rospy
 import math
 import numpy as np
 import msg_helpers
@@ -17,12 +17,7 @@ def __check_bounds(p, A, B):
     x2 = max(A[0], B[0])
     y1 = min(A[1], B[1])
     y2 = max(A[1], B[1])
-    
     return p[0] >= x1 and p[0] <= x2 and p[1] >= y1 and p[1] <= y2
-
-# угол между вектором и осью x
-def __vector_angle(vec):
-    return math.atan2(vec[1], vec[0])
 
 # Нахождение точек пересечения между окружностью и отрезком
 # p1,p2  - границы отрезка
@@ -93,16 +88,42 @@ def __find_intersections(p1, p2, O, r, eps):
 # O      - центр окружности
 # r      - радиус окружности
 # eps    - точность проверки 
-def find_path_intersections(path, O, r, eps):        
+# start  - с какого индекса надо начинать искать
+# length - какое количество точек искать, начиная со start
+def find_path_intersections(path, O, r, eps, start = 0, length = -1):        
     intersetcs = []
-    for i in range(len(path.poses)-1):        
-        p = __find_intersections(msg_helpers.point_to_array(path.poses[i].pose.position),
+    poses=path.poses[start:length]
+    print(start, length, len(poses))
+    for i in range(len(poses)-1):
+        points = __find_intersections(msg_helpers.point_to_array(path.poses[i].pose.position),
                                msg_helpers.point_to_array(path.poses[i+1].pose.position),
                                O, r, eps)
-        intersetcs.extend(p)
+
+        for p in points:
+            intersetcs.append((i, p))
     
     return intersetcs
 
+
+def __rot_matrix(angle):
+    c = np.cos(angle)
+    s = np.sin(angle)
+    return np.array([[c, -s],
+                     [s,  c]])
+
+def __rot_vector(vec, angle):
+    return np.matmul(__rot_matrix(angle), vec)
+
+def __vec_of_angle(angle):
+    return __rot_vector(np.array([1,0]), angle)
+
 # находит пересечение, угол на который меньше всего отличатеся от текущего курса
-def find_closest_intersect(pos, orientation, intersects):    
-    return  min(map(lambda x: (x, __vector_angle(x - pos) - orientation), intersects), key=lambda x: abs(x[1]))
+def find_closest_intersect(pos, orientation, intersects):  
+    
+    # Берем пересечение с самым большим
+    intersect = max(intersects, key=lambda x: x[0])
+    vec_to_intersect =  intersect[1] - pos
+    orientation_vec = __vec_of_angle(orientation)
+    bearing = math.atan2(np.cross(orientation_vec, vec_to_intersect), np.dot(vec_to_intersect, orientation_vec))
+    return (intersect[0], intersect[1], bearing)
+    #return  max(map(lambda x: (x[1], __vector_angle(x[1] - pos) - orientation, x[0]), intersects), key=lambda x: x[2])