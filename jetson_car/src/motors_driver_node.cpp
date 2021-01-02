fa3e00743c666bcf836a343c03be080b3053ab8d
nvidia <Markov Alexey>
2021-01-02 22:29:28 +0400

    Add TTY (uart) settings to the motors_driver_node
    By default TTY is 9600 baud, so, add settings directly to the node,
    instead configuring it on the system start-up via stty and some script

--- jetson_car/src/motors_driver_node.cpp
+++ jetson_car/src/motors_driver_node.cpp
@@ -10,16 +10,59 @@
 #include <stdio.h>
 #include <fcntl.h>
 #include <stdint.h>
+#include <termios.h>
+#include <errno.h>
+
+#define SERIAL "/dev/ttyTHS2"
 
 
-int8_t buffer[]={0x34, 0x27, 0x77, 0x10, 0x10};
-int com_fd;
+int8_t buffer[]={0x34, 0x27, 0x77, 0x00, 0x00};
+int serial_fd;
 
 void callback(const jetson_car::MotorsControl msg)
 {
 	buffer[3] = msg.left;
 	buffer[4] = msg.right;
-	write(com_fd, buffer, sizeof(buffer));
+	write(serial_fd, buffer, sizeof(buffer));
+}
+
+int open_and_configure_serial(const char* device)
+{
+	struct termios tty;
+
+	int fd = open(device, O_RDWR);
+	if(fd == -1) {
+		ROS_ERROR_STREAM("Unable to open UART: " << strerror(errno));
+		goto err;
+	}
+
+	if (tcgetattr(fd, &tty) != 0) {
+		ROS_ERROR_STREAM("Unable to read TTY settings: " << strerror(errno));
+		goto err;
+	}
+
+	// Baud 115200, 8N1, no parity, no HW control
+	tty.c_cflag &= ~PARENB;   // no parity
+	tty.c_cflag &= ~CSTOPB;   // 1 stop bit
+	tty.c_cflag &= ~CSIZE;    // clear number of bits
+	tty.c_cflag |= CS8;       // set 8 bits
+	tty.c_cflag &= ~CRTSCTS;  // no HW control
+	cfsetispeed(&tty, B115200);
+	// Well, there are more settings (line discipline, echo etc), but I think, this is enough.
+	// Actually, I think baud is only one we need...
+
+	if (tcsetattr(fd, TCSANOW, &tty) != 0) {
+		ROS_ERROR_STREAM("Unable to write TTY settings: " << strerror(errno));
+		goto err;
+	}
+
+	return fd;
+err:
+	if (fd != -1) {
+		close(fd);
+	}
+	return -1;
+
 }
 
 int main(int argc, char** argv)
@@ -28,10 +71,8 @@ int main(int argc, char** argv)
 	ros::NodeHandle nh;
 	ROS_INFO("Motors driver started");
 
-	com_fd = open("/dev/ttyS0", O_RDWR);
-	if(com_fd == -1)
-	{
-		ROS_ERROR("Unable to open UART");
+	if ((serial_fd = open_and_configure_serial(SERIAL)) == -1) {
+		ROS_ERROR_STREAM("Unable to open UART");
 		return -1;
 	}
 
