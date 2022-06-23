6a11a531fae23e6729a9dd1fbd7aba1fec01300b
Markov Alexey <markovalex95@gmail.com>
2022-06-23 12:26:07 +0300

    Change simple_odologger to log LiDAR via TF

--- jetson_car/scripts/simple_odologger.py
+++ jetson_car/scripts/simple_odologger.py
@@ -5,13 +5,14 @@ import time
 import math
 import tf
 from nav_msgs.msg import Odometry
-from geometry_msgs.msg import PoseStamped
+from geometry_msgs.msg import PoseStamped, Pose, Point, Quaternion
 
 ts = str(math.floor(time.time()))
 ts_dot = ts.find('.')
 if ts_dot != -1:
 	# On some systems str on floored number produces result with a dot, on some don't
 	ts = ts[:ts_dot]
+
 lidar_f = open('/home/nvidia/slogs/' + ts + '_lidar.txt', 'w')
 lidar_f_full = open('/home/nvidia/slogs/' + ts + '_lidar_full.txt', 'w')
 zed_f = open('/home/nvidia/slogs/' + ts + '_zed.txt', 'w')
@@ -20,37 +21,38 @@ lidar_f.write('X\tY\n')
 lidar_f_full.write('t\tX\tY\tZ\tYaw\tPitch\tRoll\tQ.x\tQ.y\tQ.z\tQ.w\n')
 zed_f.write('X\tY\n')
 zed_f_full.write('t\tX\tY\tZ\tYaw\tPitch\tRoll\tQ.x\tQ.y\tQ.z\tQ.w\n')
+
 T = None
 
-def do_log(f, ff, pose):
+def do_log(f, ff, timestamp, pose):
 	global T
 	if T is None:
-		T = rospy.get_time()
-	pos = pose.position
+		T = timestamp
+
 	# Short logging
+	pos = pose.position
 	f.write(str(pos.x) + '\t' + str(pos.y) + '\n')
-	# Flush ?
-	#f.flush()
+
 	# Full logging
-	dt = rospy.get_time() - T
+	dt = timestamp - T
 	q = pose.orientation  # Quaternion
 	roll, pitch, yaw = tf.transformations.euler_from_quaternion([q.w, q.x, q.y, q.z])
 	ff.write(str(dt) + '\t' + str(pos.x) + '\t' + str(pos.y) + '\t' + str(pos.z) + '\t' +
 		str(yaw) + '\t' + str(pitch) + '\t' + str(roll) + '\t' +
 		str(q.x) + '\t' + str(q.y) + '\t' + str(q.z) + '\t' + str(q.w) + '\n')
 
-def log_lidar(data):
-	do_log(lidar_f, lidar_f_full, data.pose.pose)
-
 def log_zed(data):
-	do_log(zed_f, zed_f_full, data.pose)
+	do_log(zed_f, zed_f_full, data.header.stamp, data.pose)
+        try:
+            pos, qrot = listener.lookupTransform('/map', '/base_link', rospy.Time(0))
+            pose = Pose(Point(*pos), Quaternion(*qrot))
+	    do_log(lidar_f, lidar_f_full, data.header.stamp, pose)
+        except(LookupException):
+            pass
 
-def listener():
-	global T
-	rospy.init_node('simple_odologger')
-	rospy.Subscriber('/integrated_to_init', Odometry, log_lidar)
-	rospy.Subscriber('/zed/zed_node/pose', PoseStamped, log_zed)
-	rospy.spin()
 
 if __name__ == '__main__':
-	listener()
+	rospy.init_node('simple_odologger')
+	rospy.Subscriber('/zed/zed_node/pose', PoseStamped, log_zed)
+        listener = tf.TransformListener()
+	rospy.spin()
